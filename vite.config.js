5af28e334b4f0df43eea971536e7a29dd58bddec
Masaki <mo.synthkey.0112@gmail.com>
2025-09-28 00:12:48 +0900

    updated website

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/melissa-audio/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     vue(),
     vuetify({ autoImport: true }), // Enable automatic imports
